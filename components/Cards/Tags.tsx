54ca1b2fedf8f5c5544ad0848ec05cac3b2eb0f7
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-08-11 13:48:25 +0530

    feat: implement tag input

--- components/Cards/Tags.tsx
+++ components/Cards/Tags.tsx
@@ -1,5 +1,5 @@
+import Image from "next/image";
 import Link from "next/link";
-import React from 'react';
 
 import {Badge} from "@/components/ui/badge";
 import ROUTES from "@/constants/routes";
@@ -11,28 +11,47 @@ interface TagProps {
     name: string,
     showCount?: boolean,
     questions?: number,
-    compact?: boolean
+    compact?: boolean,
+    remove?: boolean,
+    isButton?: boolean,
+    handleRemove?: () => void
 }
 
-const Tags = ({name, _id, showCount, compact, questions}: TagProps) => {
+const Tags = ({name, _id, showCount, compact, remove, isButton, handleRemove, questions}: TagProps) => {
     const iconClass = getDevIconsClassName(name);
-    return (
-        <Link href={ROUTES.TAG(_id)} className="flex justify-between gap-2">{name}
-            <Badge className="background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2">
-                <div className="flex-center space-x-2">
-                    <i className={`${iconClass}`}>
+    const Content = <>
+        <Badge
+            className="background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase flex flex-row gap-2 ">
+            <div className="flex-center space-x-2">
+                <i className={`${iconClass}`}>
 
-                    </i>
-                    <span>{name}</span>
-                </div>
-            </Badge>
-            {
-                showCount && <p className="small-medium text-dark500_light700">
-                    {questions}
-                </p>
+                </i>
+                <span>{name}</span>
+            </div>
+            {remove &&
+                <Image src="/icons/close.svg" alt="close-icon"
+                       className="cursor-pointer object-contain invert-0 dark:invert " width={12} height={12} onClick={handleRemove}/>
             }
-        </Link>
-    );
+        </Badge>
+        {
+            showCount && <p className="small-medium text-dark500_light700">
+                {questions}
+            </p>
+        }
+    </>
+
+    function handleClick(e:React.MouseEvent) {
+        e.preventDefault();
+    }
+
+    if (compact) {
+        return isButton ?
+            (<button onClick={handleClick} className="flex justify-between gap-2 ">
+                {Content}
+            </button>) : <Link href={ROUTES.TAG(_id)} className="flex justify-between gap-2">{name}
+                {Content}
+            </Link>
+    }
 };
 
 export default Tags;
